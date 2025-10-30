b545502fcd9e34aed3fe31f48ea0da915735b4e7
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-10-30 11:47:09 +0100

    Refactor: index.ts

--- src/render/index.tsx
+++ src/render/index.tsx
@@ -1,12 +1,12 @@
 import { render } from '@solid-js/web';
 import { HashRouter, Route } from '@solid-js/router'
-import { Button } from '../components/button/index.ts';
+import { button } from '../components/index.ts';
 import './index.css'
 
 
 render(
   () => <HashRouter>
-    <Route path='/button' component={() => <Button>Hello</Button>}/>
+    <Route path='/button' component={() => <button.Button>Hello</button.Button>}/>
   </HashRouter>,
   globalThis.document.body
 );
